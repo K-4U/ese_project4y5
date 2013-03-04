bbbe853cd57979de86c53c4282ea42fbc865269d
Koen Beckers <koen@k-4u.nl>
2013-03-04 14:59:08 +0100

    Imported C# project. Serial works

---
+++ Simulator/frmMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Simulator {
+    partial class frmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsLblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPortToolStripMenuItem = new System.Windows.Forms.ToolStripComboBox();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsLblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 308);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(618, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsLblStatus
+            // 
+            this.tsLblStatus.Name = "tsLblStatus";
+            this.tsLblStatus.Size = new System.Drawing.Size(117, 17);
+            this.tsLblStatus.Text = "Status: Disconnected";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serialPortToolStripMenuItem,
+            this.connectToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // serialPortToolStripMenuItem
+            // 
+            this.serialPortToolStripMenuItem.Name = "serialPortToolStripMenuItem";
+            this.serialPortToolStripMenuItem.Size = new System.Drawing.Size(152, 23);
+            this.serialPortToolStripMenuItem.Text = "Serial Port:";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.connectToolStripMenuItem.Text = "&Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(209, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 330);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsLblStatus;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox serialPortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Simulator/frmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulator {
+    enum logTags {
+        program = ConsoleColor.Green,
+        serial,
+        serialServer
+    }
+
+    public partial class frmMain : Form {
+        private static serialServer serSock;
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
+
+        static void log(string x, logTags tag) {
+            string lTag = "";
+            if (tag == logTags.program) {
+                lTag = "PROG";
+            } else if (tag == logTags.serialServer) {
+                lTag = "SERI";
+            } else if (tag == logTags.serial) {
+                lTag = "BYTE";
+            }
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = (ConsoleColor)tag;
+            //Console.Write(String.Format("[{0}]\t", lTag));
+            Console.WriteLine(String.Format("[{0}]\t{1}", lTag, x));
+            Console.ForegroundColor = oldColor;
+
+        }
+
+        public frmMain() {
+            InitializeComponent();
+            //Load serial server:
+            serSock = new serialServer();
+            serSock.setLogFunction(log);
+
+            serSock.setMessageHandler(messageHandlerSocket);
+        }
+
+        private void messageHandlerSocket(int bRead, int prevByte) {
+        }
+
+        private void frmMain_Load(object sender, EventArgs e) {
+            //Find the serial port names
+            foreach (COMPortInfo comPort in COMPortInfo.GetCOMPortsInfo()) {
+                serialPortToolStripMenuItem.Items.Add(comPort.Description);
+                Console.WriteLine(string.Format("{0} – {1}", comPort.Name, comPort.Description));
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e) {
+            if (serSock.isConnected()) {
+                serSock.disconnect();
+            }
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e) {
+            String selectedPort = "";
+            foreach (COMPortInfo comPort in COMPortInfo.GetCOMPortsInfo()) {
+                if (comPort.Description == serialPortToolStripMenuItem.SelectedItem.ToString()) {
+                    selectedPort = comPort.Name;
+                }
+            }
+
+            serSock.init(selectedPort, Properties.Settings.Default.baudRate);
+
+            try {
+                serSock.connect();
+            } catch (serialException exc) {
+                log(String.Format("ERROR: {0}\r\nStack{1}", exc.Message, exc.StackTrace), logTags.serialServer);
+            }
+        }
+    }
+}

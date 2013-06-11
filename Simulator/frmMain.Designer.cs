8eb0a4abe17b090a0b627cd8316d3fa1ee1fc722
Leon Gersen <leongersen@gmail.com>
2013-06-11 11:40:35 +0200

    Processrapport naar html

--- Simulator/frmMain.Designer.cs
+++ Simulator/frmMain.Designer.cs
@@ -190,10 +190,10 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Engine,
             this.State});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Location = new System.Drawing.Point(1148, 36);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(244, 107);
+            this.dataGridView1.Size = new System.Drawing.Size(244, 219);
             this.dataGridView1.TabIndex = 3;
             // 
             // Engine
